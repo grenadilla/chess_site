b6904c8cc64289ccdf000f67f8a17997e724389b
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2018-07-10 21:21:25 +0000

    Added player and game list pages

--- statdisplay/urls.py
+++ statdisplay/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('players', views.player_list, name='player_list'),
     path('players/<user_name>', views.players, name='players'),
+    path('games', views.game_list, name='game_list'),
     path('games/<game_id>', views.games, name='games'),
 ]
--- statdisplay/views.py
+++ statdisplay/views.py
@@ -8,11 +8,22 @@ from statdisplay.models import Player, Game
 def index(request):
     return HttpResponse("Hello, world. You're at the statdisplay index.")
 
+def player_list(request):
+    players = Player.objects.all().order_by('user_name')
+    context = {'players': players}
+    return render(request, "statdisplay/player_list.html", context)
+
 def players(request, user_name):
     player = Player.objects.filter(user_name__iexact=user_name).first()
-    games = (Game.objects.filter(player_white=player) | Game.objects.filter(player_black=player)).distinct()
+    games = (Game.objects.filter(player_white=player) | 
+            Game.objects.filter(player_black=player)).distinct().order_by('-date_finished')
     context = {'player': player, 'games': games}
     return render(request, "statdisplay/player.html", context)
+
+def game_list(request):
+    games = Game.objects.all().order_by('-date_finished')
+    context = {'games': games}
+    return render(request, "statdisplay/game_list.html", context)
 
 def games(request, game_id):
     game = Game.objects.filter(game_id=game_id).first()
