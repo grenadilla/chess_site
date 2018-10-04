d34ff4d81c842cdeb45a63b59c094b722e20958b
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2018-10-04 13:11:03 +0000

    Updated stattracker.py to use list of player games instead of activity

--- statdisplay/stattracker.py
+++ statdisplay/stattracker.py
@@ -1,5 +1,5 @@
 import os, sys
-import requests, json
+import requests, json, datetime
 import django
 
 sys.path.append('..')
@@ -9,8 +9,15 @@ django.setup()
 from statdisplay.models import Player, Game
 
 TEAM_NAME = "vhhs"
+
+YESTERDAY = datetime.date.today() - datetime.timedelta(1)
+YESTERDAY = datetime.datetime.combine(YESTERDAY, datetime.datetime.min.time())
+EPOCH = datetime.datetime.utcfromtimestamp(0)
+UNIX_TIME = int((YESTERDAY - EPOCH).total_seconds() * 1000.0)
+
 print("Checking for new team members")
-resp = requests.get("https://lichess.org/team/" + TEAM_NAME + "/users")
+url = "https://lichess.org/team/" + TEAM_NAME + "/users"
+resp = requests.get(url)
 if resp.status_code == 200:
     print("Status code 200")
     for line in resp.text.splitlines():
@@ -29,42 +36,50 @@ if resp.status_code == 200:
             print("Added new player " + new_player.user_name)
 
 for player in Player.objects.all():
-    print("Checking player " + player.user_name)
-    resp = requests.get("https://lichess.org/api/user/" + player.user_name + "/activity")
+    url = ("https://lichess.org/api/games/user/" + 
+           player.user_name + 
+           "?moves=false?tags=false?since=" + 
+           str(UNIX_TIME))
+    print("Checking player " + player.user_name + " using url: " + url)
+    resp = requests.get(url, headers={"Accept": "application/x-ndjson"})
+    if resp.status_code == 200:
+        print("Status code 200")
+        print("Checking games")
+        for line in resp.text.splitlines():
+            data = json.loads(line)
+            if "user" not in data["players"]["white"] or "user" not in data["players"]["black"]:
+                continue
+            if data["players"]["white"]["user"]["name"] == player.user_name:
+                opponent_color = "black"
+            else:
+                opponent_color = "white"
+            opponent = Player.objects.filter(user_name__iexact=data["players"][opponent_color]["user"]["name"])
+            if not Game.objects.filter(game_id=data["id"]).exists() and opponent.exists():
+                if opponent_color == "black":
+                    white = player
+                    black = opponent.first()
+                else:
+                    white = opponent.first()
+                    black = player
+                new_game = Game(game_id=data["id"], player_white=white, player_black=black)
+                new_game.save()
+                new_game.players.add(white)
+                new_game.players.add(black)
+                print("Added new game " + new_game.game_id)
+print("Checking finished games")
+for game in Game.objects.filter(complete=False):
+    url = "https://lichess.org/api/game/" + game.game_id
+    resp = requests.get(url)
     if resp.status_code == 200:
         print("Status code 200")
         data = resp.json()
-        if "correspondenceMoves" in data[0]:
-            print("Checking games")
-            for game in data[0]["correspondenceMoves"]["games"]:
-                opponent = Player.objects.filter(user_name__iexact=game["opponent"]["user"])
-                if not Game.objects.filter(game_id=game["id"]).exists() and opponent.exists():
-                    if game["color"] == "white":
-                        white = player
-                        black = opponent.first()
-                    else:
-                        white = opponent.first()
-                        black = player
-                    new_game = Game(game_id=game["id"], player_white=white, player_black=black)
-                    new_game.save()
-                    new_game.players.add(white)
-                    new_game.players.add(black)
-                    print("Added new game " + new_game.game_id)
-        if "correspondenceEnds" in data[0]:
-            print("Checking finished games")
-            for game in data[0]["correspondenceEnds"]["games"]:
-                resp = requests.get("https://lichess.org/api/game/" + game["id"])
-                if resp.status_code == 200:
-                    print("Status code 200")
-                    data = resp.json()
-                    game_object = Game.objects.filter(game_id=data["id"]).first()
-                    if not game_object.complete:
-                        game_object.complete = True
-                        if data["winner"] == "white":
-                            game_object.winner_id = 1
-                        elif data["winner"] == "black":
-                            game_object.winner_id = 2
-                        else:
-                            game_object.winner_id = 0
-                        game_object.save()
-                        print("Updated finished game " + game_object.game_id)
+        if not data["status"] == "started":
+            game.complete = True
+            if data["status"] == "draw":
+                game.winner_id = 0
+            elif "winner" in data and data["winner"] == "black":
+                game.winner_id = 2
+            elif "winner" in data and data["winner"] == "white":
+                game.winner_id = 1
+            game.save()
+            print("Updated finished game " + game.game_id)
