f3710b926ef2ff0ddfb7fe4217b16e44367f7e48
Jeffrey Zhang <jeffreyzhang01@gmail.com>
2018-12-16 02:48:43 +0000

    Added form for selecting players to compare

---
+++ statdisplay/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from statdisplay.models import Player
+
+def get_compare_choices():
+    players = Player.objects.all()
+    choices = []
+    for player in players:
+        choices.append((player.user_name, player.user_name))
+    return choices
+
+class CompareForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(CompareForm, self).__init__(*args, **kwargs)
+        self.fields['player1'] = forms.ChoiceField(choices=get_compare_choices())
+        self.fields['player2'] = forms.ChoiceField(choices=get_compare_choices())
+
